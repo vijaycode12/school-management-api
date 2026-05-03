4f47e424b74b5c33af540ab183b193452c2ccae9
vijaycode12 <reddyvijay4043@gmail.com>
2026-05-03 11:29:46 +0530

    Updated

--- src/config/env.js
+++ src/config/env.js
@@ -2,13 +2,13 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const {
-    PORT,
-    NODE_ENV,
-    BACKEND_URL,
-    DB_HOST,
-    DB_PORT,
-    DB_USER,
-    DB_PASSWORD,
-    DB_NAME,
-} = process.env;
+export const PORT        = process.env.PORT;
+export const NODE_ENV    = process.env.NODE_ENV;
+export const BACKEND_URL = process.env.BACKEND_URL;
+
+
+export const DB_HOST     = process.env.MYSQLHOST      || process.env.DB_HOST;
+export const DB_USER     = process.env.MYSQLUSER      || process.env.DB_USER;
+export const DB_PASSWORD = process.env.MYSQLPASSWORD  || process.env.DB_PASSWORD;
+export const DB_NAME     = process.env.MYSQL_DATABASE || process.env.DB_NAME;
+export const DB_PORT     = process.env.MYSQLPORT      || process.env.DB_PORT || 3306;
